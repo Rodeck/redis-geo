f455dcb071b968d8f4bf742612c597ae3a798033
Gabriel Pajak <pajak.gabriel@gmail.com>
2020-02-14 16:16:33 +0100

    Rename cluster

--- deploy-envs.sh
+++ deploy-envs.sh
@@ -5,7 +5,7 @@ export IMAGE_NAME=netcoreapps-redisgeo
 export IMAGE_VERSION=latest
 
 export AWS_DEFAULT_REGION=eu-west-1
-export AWS_ECS_CLUSTER_NAME=default
+export AWS_ECS_CLUSTER_NAME=DoItCI
 export AWS_VIRTUAL_HOST=34.247.93.138
 
 # set any sensitive information in travis-ci encrypted project settings:
